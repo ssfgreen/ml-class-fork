0e33f31db94b5e666041784b704cab7bc5f7ce48
Sam Green <samsfgreen@gmail.com>
2023-01-31 11:45:24 +0000

    adding conda env exports that work locally on M1 Macbook Pro

--- videos/intro/perceptron-linear.py
+++ videos/intro/perceptron-linear.py
@@ -24,14 +24,12 @@ labels = range(10)
 num_classes = y_train.shape[1]
 
 # create model
-model=Sequential()
-model.add(Flatten(input_shape=(img_width,img_height)))
+model = Sequential()
+model.add(Flatten(input_shape=(img_width, img_height)))
 model.add(Dense(num_classes))
 model.compile(loss='mse', optimizer='adam',
-                metrics=['accuracy'])
+              metrics=['accuracy'])
 
 # Fit the model
 model.fit(X_train, y_train, epochs=10, validation_data=(X_test, y_test),
-                    callbacks=[WandbCallback(labels=labels, data_type="image")])
-
-
+          callbacks=[WandbCallback(labels=labels, data_type="image")])
